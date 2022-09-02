8035fb45555840fa6b0e82334fef43af76bb9751
kodsurfer <kodsurfer@ya.ru>
2022-09-03 02:09:14 +0300

    Initial commit

---
+++ slices-intersection/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{1, 2, 3, 4, 5}
+	b := []int{3, 4, 5, 6, 7}
+
+	fmt.Printf("%v and %v intersection: %v \n", a, b, insersection(a, b))
+
+	a = []int{1, 2, 1, 2, 1}
+	b = []int{3, 2, 1}
+
+	fmt.Printf("%v and %v intersection: %v \n", a, b, insersection(a, b))
+}
+
+func insersection(a, b []int) []int {
+	cntr := make(map[int]int)
+	res := make([]int, 0)
+
+	for _, v := range a {
+		if _, ok := cntr[v]; !ok {
+			cntr[v] = 1
+		} else {
+			cntr[v]++
+		}
+	}
+
+	for _, v := range b {
+		if cnt, ok := cntr[v]; ok && cnt > 0 {
+			cntr[v]--
+			res = append(res, v)
+		}
+	}
+
+	return res
+}
